d8189621bf146fac7a19c95fea05f8644c952825
YaoveiD <1679498957@qq.com>
2021-09-26 18:43:23 +0800

    generalize

--- string/stringen.cpp
+++ string/stringen.cpp
@@ -2,14 +2,28 @@
 *  author: yaoveil
 *  date:   2021-06-22 23:19:24
 */
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+const char MIN_CHAR = 'a';
+const char MAX_CHAR = 'z';
+const int ALPHABET = 26;
+
 template<class T>
 class StringGen {
 public:
   string itostr(T x) {
     string res;
     while (x) {
-      int m = x % 26;
-      res += (m == 0 ? 'z' : 'a' + m - 1);
+      int m = x % ALPHABET;
+      res += (m == 0 ? MAX_CHAR : MIN_CHAR + m - 1);
       if (m == 0) {
         --x;
       }
@@ -22,7 +36,7 @@ public:
   T strtoi(const string& s) {
     T res = 0;
     for (int i = 0; i < (int)s.size(); ++i) {
-      res = res * 26 + s[i] - 'a' + 1;
+      res = res * ALPHABET + s[i] - MIN_CHAR + 1;
     }
     return res;
   }
@@ -30,8 +44,8 @@ public:
   void add(string& s) {
     int inc = 1;
     for (int i = (int)s.size() - 1; i >= 0 && inc; --i) {
-      if (s[i] + inc > 'z') {
-        s[i] = 'a';
+      if (s[i] + inc > MAX_CHAR) {
+        s[i] = MIN_CHAR;
       }
       else {
         s[i]++;
@@ -39,11 +53,11 @@ public:
       }
     }
     if (inc) {
-      s = 'a' + s;
+      s = MIN_CHAR + s;
     }
   }
 
-  void gen(vector<string>& strings, int sz, string start = "a") {
+  void gen(vector<string>& strings, int sz, string start = string(1, MIN_CHAR)) {
     for (int i = 0; i < sz; ++i) {
       strings.push_back(start);
       add(start);
