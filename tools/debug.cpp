69348c73c96a4599bc85341cb94ca4b3cffc84b0
YaoveiD <1679498957@qq.com>
2021-10-07 19:14:25 +0800

    update debug

--- tools/debug.cpp
+++ tools/debug.cpp
@@ -1,31 +1,8 @@
-//https://github.com/the-tourist/algo/blob/master/misc/debug.cpp
-
-string to_string(string s) { return '"' + s + '"'; }
-string to_string(const char* s) { return to_string((string) s); }
-string to_string(bool b) { return (b ? "true" : "false"); }
-template <typename A, typename B>
-string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
-string to_string(vector<bool> v) {
-    bool first = true; string res = "{";
-    for (int i = 0; i < static_cast<int>(v.size()); i++) {
-        if (!first) res += ", ";
-        first = false;
-        res += to_string(v[i]);
-    }
-    res += "}"; return res;
-}
-template <typename iterable_T>
-string to_string(iterable_T v) {
-    bool first = true; string res = "{";
-    for (const auto& x : v) {
-        if (!first) res += ", ";
-        first = false; res += to_string(x);
-    }
-    res += "}"; return res;
-}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
+ 
 void debug_out() { cerr << endl; }
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T) { cerr << ' ' << to_string(H); debug_out(T...); }
+template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << ' ' << H; debug_out(T...); }
 #ifdef LOCAL
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
