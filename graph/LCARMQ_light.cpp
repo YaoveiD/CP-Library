7a571907944b5dc8602c7f4f020c1b98a6a6a048
YaoveiD <1679498957@qq.com>
2021-11-13 20:59:56 +0800

    add light LCA

---
+++ graph/LCARMQ_light.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+ 
+using namespace std;
+
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template<class Fun> class y_combinator_result {
+    Fun fun_;
+public:
+    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
+ 
+void debug_out() { cerr << endl; }
+template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << ' ' << H; debug_out(T...); }
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 
+#endif
+
+// source : https://codeforces.com/contest/1328/submission/74421058 ksun
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+
+template<class T>
+struct RMQ {
+    vector<vector<T>> mat;
+    
+    RMQ(const vector<T>& V) : mat(1, V) {
+        for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
+            mat.emplace_back(sz(V) - pw * 2 + 1);
+            rep(j,0,sz(mat[k]))
+                mat[k][j] = min(mat[k - 1][j], mat[k - 1][j + pw]);
+        }
+    }
+    
+    T query(int a, int b) {
+        assert(a < b); // or return inf if a == b
+        int dep = 31 - __builtin_clz(b - a);
+        return min(mat[dep][a], mat[dep][b - (1 << dep)]);
+    }
+};
+ 
+struct LCA {
+    int T = 0;
+    vi time, path, ret;
+    vector<int> parent;
+    RMQ<int> rmq;
+ 
+    LCA(vector<vi>& C) : time(sz(C)), parent(sz(C)), rmq((dfs(C,0,-1), ret)) {}
+    void dfs(vector<vi>& C, int v, int par) {
+        parent[v] = par;
+        time[v] = T++;
+        trav(y, C[v]) if (y != par) {
+            path.push_back(v), ret.push_back(time[v]);
+            dfs(C, y, v);
+        }
+    }
+ 
+    int lca(int a, int b) {
+        if (a == b) return a;
+        tie(a, b) = minmax(time[a], time[b]);
+        return path[rmq.query(a, b)];
+    }
+    // dist(a,b){return depth[a] + depth[b] - 2*depth[lca(a,b)];}
+};
