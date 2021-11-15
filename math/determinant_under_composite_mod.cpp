1f0539a613c5a49ca966885aa342cdae3df0def6
YaoveiD <1679498957@qq.com>
2021-11-15 12:25:45 +0800

    add guass

--- math/determinant_under_composite_mod.cpp
+++ math/determinant_under_composite_mod.cpp
@@ -5,9 +5,9 @@ const int N = 3e5 + 9;
 
 // source : https://github.com/ShahjalalShohag/code-library/blob/master/Math/
 // solition to https://www.luogu.com.cn/problem/P7112
-// returns the det of a under composite modular in O(n^3 logn) time. maybe faster.
+// returns the det of a under composite MODular in O(n^3 logn) time. maybe faster.
 
-int Gauss(vector<vector<int>> a, const int mod) {
+int Gauss(vector<vector<int>> a, const int MOD) {
   int n = (int)a.size();
   if (n != a[0].size())
     return 0;
@@ -22,42 +22,42 @@ int Gauss(vector<vector<int>> a, const int mod) {
     for (int i = col; i < n; i++)
       swap(a[row][i], a[mx][i]);
     if (row != mx)
-      det = det == 0 ? 0 : mod - det;
+      det = det == 0 ? 0 : MOD - det;
     for (int i = row + 1; i < n; i++) {
       while (a[row][col]) {
         int t = a[i][col] / a[row][col];
         for (int j = col; j < n; j++) {
-          a[i][j] -= 1LL * a[row][j] * t % mod;
+          a[i][j] -= 1LL * a[row][j] * t % MOD;
           if (a[i][j] < 0)
-            a[i][j] += mod;
+            a[i][j] += MOD;
           swap(a[i][j], a[row][j]);
         }
-        det = det == 0 ? 0 : mod - det;
+        det = det == 0 ? 0 : MOD - det;
       }
       for (int j = col; j < n; j++)
         swap(a[row][j], a[i][j]);
-      det = det == 0 ? 0 : mod - det;
+      det = det == 0 ? 0 : MOD - det;
     }
-    det = 1LL * det * a[row][col] % mod;
+    det = 1LL * det * a[row][col] % MOD;
     ++row;
   }
   return det;
 }
 int32_t main() {
-  int n, mod;
-  while (cin >> n >> mod) {
+  int n, MOD;
+  while (cin >> n >> MOD) {
     vector<vector<int>> a(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
         int k;
         cin >> k;
-        k %= mod;
+        k %= MOD;
         if (k < 0)
-          k += mod;
+          k += MOD;
         a[i][j] = k;
       }
     }
-    cout << Gauss(a, mod) << '\n';
+    cout << Gauss(a, MOD) << '\n';
   }
   return 0;
 }
--- math/determinant_under_prime_mod.cpp
+++ math/determinant_under_prime_mod.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 105, mod = 998244353;
+const int N = 105, MOD = 998244353;
 
 int power(long long n, long long k) {
-  int ans = 1 % mod;
-  n %= mod;
+  int ans = 1 % MOD;
+  n %= MOD;
   if (n < 0)
-    n += mod;
+    n += MOD;
   while (k) {
     if (k & 1)
-      ans = (long long)ans * n % mod;
-    n = (long long)n * n % mod;
+      ans = (long long)ans * n % MOD;
+    n = (long long)n * n % MOD;
     k >>= 1;
   }
   return ans;
 }
 
 // source : https://github.com/ShahjalalShohag/code-library/blob/master/Math/Determinant%20under%20Prime%20Modulo.cpp
-// returns the determinant of a under prime modular.
+// returns the determinant of a under prime MODular.
 
 int Gauss(vector<vector<int>> a) {
   int n = a.size(), m = (int)a[0].size();
-  const long long MODSQ = (long long)mod * mod;
+  const long long MODSQ = (long long)MOD * MOD;
   int det = 1, rank = 0;
   for (int col = 0, row = 0; col < m && row < n; col++) {
     int mx = row;
@@ -36,15 +36,15 @@ int Gauss(vector<vector<int>> a) {
     for (int j = col; j < m; j++)
       swap(a[mx][j], a[row][j]);
     if (row != mx)
-      det = det == 0 ? 0 : mod - det;
-    det = 1LL * det * a[row][col] % mod;
-    int inv = power(a[row][col], mod - 2);
+      det = det == 0 ? 0 : MOD - det;
+    det = 1LL * det * a[row][col] % MOD;
+    int inv = power(a[row][col], MOD - 2);
     for (int i = 0; i < n && inv; i++) {
       if (i != row && a[i][col]) {
-        int x = ((long long)a[i][col] * inv) % mod;
+        int x = ((long long)a[i][col] * inv) % MOD;
         for (int j = col; j < m && x; j++) {
           if (a[row][j])
-            a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % mod;
+            a[i][j] = (MODSQ + a[i][j] - ((long long)a[row][j] * x)) % MOD;
         }
       }
     }
