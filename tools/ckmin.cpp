4e9752c1fe1217590b25b445fb1cff83c8b33f62
YaoveiD <1679498957@qq.com>
2021-11-23 18:37:08 +0800

    add basic thighs

--- tools/ckmin.cpp
+++ tools/ckmin.cpp
@@ -1,3 +1,3 @@
 
-template<class T> inline bool ckmin(T& a, const T& b) { return b < a ? a = b, true : false; }
-template<class T> inline bool ckmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+template<class T> inline bool setmin(T& a, T b) { return b < a ? a = b, true : false; }
+template<class T> inline bool setmax(T& a, T b) { return a < b ? a = b, true : false; }
