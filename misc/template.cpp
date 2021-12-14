b7a105dd963a93c5309c37c82d0bf1fa5543933d
YaoveiD <1679498957@qq.com>
2021-12-14 12:42:39 +0800

    rewrite template

--- misc/template.cpp
+++ misc/template.cpp
@@ -1,40 +1,8 @@
 #include <algorithm>
-#include <array>
-#include <bitset>
 #include <cassert>
-#include <chrono>
-#include <cmath>
-#include <cstring>
-#include <functional>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
 #include <vector>
 using namespace std;
-
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<int64_t> vl;
-
-#define FOR(i, a, b) for (int i=a; i<(b); ++i)
-#define F0R(i, a) for (int i=0; i<(a); ++i)
-#define each(i, C) for (auto &i : C)
-#define sz(x) int((x).size())
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define ins insert
- 
-vi range(int n, int start = 0) { vi r(n); iota(all(r), start); return r; }
-template<class T> bool setmin(T& a, T b) { return b < a ? a = b, true : false; }
-template<class T> bool setmax(T& a, T b) { return a < b ? a = b, true : false; }
 
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
@@ -46,7 +14,7 @@ template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { ce
 #else
 #define debug(...)
 #endif
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
--- misc/y_combinatory.cpp
+++ misc/y_combinatory.cpp
@@ -1,5 +1,19 @@
-#include <bits/stdc++.h>
-
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
 using namespace std;
 
 // http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
@@ -11,30 +25,13 @@ public:
 };
 template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
-int main() {
-    auto gcd = y_combinator([&](auto gcd, int a, int b) -> int {
-        return b == 0 ? a : gcd(b, a % b);
-    });
-    cout << gcd(4, 6) << '\n';
-
-    cout << y_combinator([&](auto gcd, int a, int b) -> int {
-        return b == 0 ? 0 : gcd(b, a % b);
-    })(4, 6) << '\n';
-
-    auto GCD = [](auto& self, int a, int b) -> int {
-        return b == 0 ? a : self(self, b, a % b);
-    };
-    cout << GCD(GCD, 4, 6) << '\n'; 
-
-    auto func = y_combinator([&](auto func, int n) -> void {
-        if (n < 0) {
-            cout << "HAHA" << '\n';
-            return;
-        }
-        func(n - 1);
-    });
-
-    func(10);
-
-    return 0;
-}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
+ 
+void debug_out() { cerr << endl; }
+template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << ' ' << H; debug_out(T...); }
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 
+#endif
