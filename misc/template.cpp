e9571e82535e201ef23fc9c718a8ddb650625380
YaoveiD <1679498957@qq.com>
2021-12-15 19:04:20 +0800

    remove debug from template

--- misc/template.cpp
+++ misc/template.cpp
@@ -4,20 +4,13 @@
 #include <vector>
 using namespace std;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
-
-void debug_out() { cerr << endl; }
-template<typename Head, typename... Tail> void debug_out(Head H, Tail... T) { cerr << ' ' << H; debug_out(T...); }
-#ifdef LOCAL
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-#else
-#define debug(...)
-#endif
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int tests; cin >> tests; while (tests--) run_case();
+    int tests;
+    cin >> tests;
+
+    while (tests--)
+        run_case();
 }
