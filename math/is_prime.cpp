6a610d8ed3ca332fc8dc60581fc9f1de01849b90
YaoveiD <1679498957@qq.com>
2022-02-22 19:56:27 +0800

    update some files

--- math/is_prime.cpp
+++ math/is_prime.cpp
@@ -11,3 +11,32 @@ bool is_prime(int64_t n) {
 
     return true;
 }
+
+// source : https://github.com/nealwu/competitive-programming/blob/master/number_theory/sieve_linear.cc
+vector<int> smallest_factor;
+vector<bool> prime;
+vector<int> primes;
+
+// Note: this sieve is O(n), but the constant factor is worse than the O(n log log n) sieve due to the multiplication.
+void sieve(int maximum) {
+    maximum = max(maximum, 1);
+    smallest_factor.assign(maximum + 1, 0);
+    prime.assign(maximum + 1, true);
+    prime[0] = prime[1] = false;
+    primes = {};
+
+    for (int i = 2; i <= maximum; i++) {
+        if (prime[i]) {
+            smallest_factor[i] = i;
+            primes.push_back(i);
+        }
+
+        for (int p : primes) {
+            if (p > smallest_factor[i] || int64_t(i) * p > maximum)
+                break;
+
+            prime[i * p] = false;
+            smallest_factor[i * p] = p;
+        }
+    }
+}
--- misc/template.cpp
+++ misc/template.cpp
@@ -4,18 +4,9 @@
 #include <vector>
 using namespace std;
 
-typedef int64_t LL;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<LL> vl;
-
-#define F0R(i, n) for (int i = 0; i < (n); ++i)
-#define FOR(i, a, b) for (int i = a; i < (b); ++i)
 #define sz(c) int(c.size())
 #define all(c) c.begin(), c.end()
 
-template<class T> inline bool setmin(T& a, T b) { return b < a ? a = b, true : false; }
-template<class T> inline bool setmax(T& a, T b) { return a < b ? a = b, true : false; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
 
