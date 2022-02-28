2596e11a5fd6d0ef7ee0d361ed385d7c2439ed4e
YaoveiD <1679498957@qq.com>
2022-02-28 10:41:39 +0800

    remove some files

--- io/output_vector.cpp
+++ io/output_vector.cpp
@@ -2,7 +2,6 @@ template<typename T, char sep = ' '>
 void output_vector(const vector<T>& v, bool add_one = false, int start = -1, int end = -1) {
     if (start < 0) start = 0;
     if (end < 0) end = int(v.size());
-
     for (int i = start; i != end; ++i)
         cout << (add_one ? v[i] + 1 : v[i]) << (i != end - 1 ? sep : '\n');
 }
