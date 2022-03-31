ba2a50a654569b23b6d145888bf545f52e55af2c
YaoveiD <1679498957@qq.com>
2022-03-31 19:32:02 +0800

    update spfa.cpp

--- graph/spfa.cpp
+++ graph/spfa.cpp
@@ -1,85 +1,72 @@
-/*
-*  author: yaoveil
-*  date:   2021-06-22 23:15:32
-*/
-template<typename T>
-class Spfa {
-public:
-  int N;
-  vector<ll> dis;
-  vector<ll> times;
+#include <bits/stdc++.h>
+using namespace std;
 
-  Spfa(int _N) {
-    N = _N;
-    dis.assign(N, inf);
-    times.assign(N, 0);
-  }
+template <typename edge_t, typename dist_t>
+struct SPFA {
+    int n;
+    vector<vector<pair<int, edge_t>>> adj;
+    vector<int> parent;
+    vector<int> times;
+    vector<bool> inque;
+    vector<dist_t> dist;
 
-  //a simple graph
-  void spfa(vector<vector<pair<int, T>>>& edge, int source = 0) {
-    queue<int> q;
-    vector<bool> vis(N, false);
-    dis[source] = 0;
-    q.push(source);
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      vis[u] = false;
-      for (auto e : edge[u]) {
-        int v = e.first;
-        if (dis[v] > dis[u] + e.second) {
-          dis[v] = dis[u] + e.second;
-          if (!vis[v] && times[v] < N) {
-            times[v]++;
-            q.push(v);
-            vis[v] = true;
-          }
-        }
-      }
+    SPFA(int _n = -1) {
+        if (_n > 0)
+            init(_n);
     }
-  }
 
-  void spfa(const graph<T>& g, int source = 0) {
-    queue<int> q;
-    vector<bool> vis(N, false);
-    dis[source] = 0;
-    q.push(source);
-    while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      vis[u] = false;
-      for (int id : g.g[u]) {
-        auto e = g.edges[id];
-        int v = e.from ^ e.to ^ u;
-        if (dis[v] > dis[u] + e.cost) {
-          dis[v] = dis[u] + e.cost;
-          if (!vis[v] && times[v] < N) {
-            times[v]++;
-            q.push(v);
-            vis[v] = true;
-          }
-        }
-      }
+    SPFA(const vector<vector<pair<int, edge_t>>> &_adj)
+        : n(int(_adj.size())), adj(_adj) {}
+
+    void init(int _n) {
+        n = _n;
+        adj.assign(n, vector<pair<int, edge_t>>());
     }
-  }
+
+    void init(const vector<vector<pair<int, edge_t>>> &_adj) {
+        n = int(_adj.size());
+        adj = _adj;
+    }
+
+    void add_edge(int a, int b, edge_t c) {
+        adj[a].emplace_back(b, c);
+    }
+
+    // returns true if negtive circle detected
+    bool solve(int src) {
+        static const dist_t INF = numeric_limits<dist_t>::max() / 2; // Note: dummy
+        dist.assign(n, INF);
+        parent.assign(n, -1);
+        times.assign(n, 0);
+        inque.assign(n, false);
+        queue<int> q;
+        dist[src] = 0;
+        times[src] = 1;
+        q.push(src);
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            inque[node] = false;
+
+            for (auto &e : adj[node]) {
+                int to = e.first;
+
+                if (dist[to] > dist[node] + e.second) {
+                    dist[to] = dist[node] + e.second;
+                    parent[to] = node;
+
+                    if (!inque[to]) {
+                        if (++times[to] > n)
+                            return true;
+
+                        q.push(to);
+                        inque[to] = true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 };
-
-template<typename T>
-bool BF(const graph<T>& g, vector<T>& dist, int src = 0) {
-  int N = g.n;
-  dist[src] = 0;
-  for (int i = 0; i <= N; ++i) {
-    for (int j = 0; j < (int) g.edges.size(); ++j) {
-      int from = g.edges[j].from;
-      int to = g.edges[j].to;
-      int cost = g.edges[j].cost;
-      if (dist[to] > dist[from] + cost) {
-        dist[to] = dist[from] + cost;
-        if (i == N) {
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-}
